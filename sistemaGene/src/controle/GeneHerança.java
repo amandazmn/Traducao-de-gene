d037fb306902297e179ebdeb45f75c94ffc1b3af
Vitória Lemos Soares <124809692+vitorialmssrs@users.noreply.github.com>
2023-06-06 20:51:23 -0300

    Add files via upload

---
+++ sistemaGene/src/controle/GeneHerança.java
@@ -0,0 +1,11 @@
+package controle;
+
+import Modelo.Gene;
+
+public class GeneHerança extends Gene {
+	
+	
+	//aqui devemos colocar classes que irão receber os "dados", ou seja criar a classe com herança. 
+	//Só colocar quais são as classes com heranças. 
+
+}
