1bf767de55d45dd8a7b75790d4bfc30520b5e6f7
Gabriely Dias <gabriely.d@aluno.ifsc.edu.br>
2023-06-13 18:37:25 -0300

    estudo de caso 04

---
+++ sistemaGene04/src/modelo/IGeneDAO.java
@@ -0,0 +1,17 @@
+package modelo;
+
+import java.util.ArrayList;
+
+public interface IGeneDAO {
+	
+	public boolean inserir(Gene gene);
+	
+	public ArrayList<Gene> listarGenes();
+	
+	public boolean alterar(Gene gene, String novaSequencia);
+	
+	public boolean excluir(Gene gene);
+	
+	public boolean exibirTabela();
+	
+}
